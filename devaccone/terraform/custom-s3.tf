2563789e39d51fb026e0e1f8160b2cf7ea72cc61
Zeeshan <zee_amd@rediff.com>
2022-09-21 08:46:09 +0000

    remove duplicate entries

--- devaccone/terraform/custom-s3.tf
+++ devaccone/terraform/custom-s3.tf
@@ -1,11 +1,9 @@
 #This custom file to test account customizations
 
-data "aws_caller_identity" "current" {}
-
 resource "aws_s3_bucket" "custom-bucket" {
-  bucket = data.aws_caller_identity.current.account_id-DoNotDel
+  bucket = "${data.aws_caller_identity.current.account_id}-DoNotDel"
 
   tags = {
-    Name = data.aws_caller_identity.current.account_id
+    Name = "${data.aws_caller_identity.current.account_id}-DoNotDel"
   }
 }
