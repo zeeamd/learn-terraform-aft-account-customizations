92f02601694dd4f0dfa6686de83e9a2f6c763a19
Zeeshan <zee_amd@rediff.com>
2022-09-21 09:23:52 +0000

    modify bucket name

--- devaccone/terraform/custom-s3.tf
+++ devaccone/terraform/custom-s3.tf
@@ -1,9 +1,9 @@
 #This custom file to test account customizations
 
 resource "aws_s3_bucket" "custom-bucket" {
-  bucket = "${data.aws_caller_identity.current.account_id}-DoNotDel"
+  bucket = "${data.aws_caller_identity.current.account_id}-donotdelete"
 
   tags = {
-    Name = "${data.aws_caller_identity.current.account_id}-DoNotDel"
+    Name = "${data.aws_caller_identity.current.account_id}-donotdelete"
   }
 }
