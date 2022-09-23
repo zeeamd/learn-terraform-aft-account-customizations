95f60ff17579dbbf2b2c0d4f94f4a206908e3bf0
Zeeshan <zee_amd@rediff.com>
2022-09-23 14:56:54 +0000

    update inline policy

--- devaccone/terraform/custom-permission-set.tf
+++ devaccone/terraform/custom-permission-set.tf
@@ -35,13 +35,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "cpa1" {
 }
 
 resource "aws_ssoadmin_permission_set_inline_policy" "cipa1" {
-  inline_policy      = file("files/policies/sqs-list.json")
-  instance_arn       = aws_ssoadmin_permission_set.cps1.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.cps1.arn
-}
-
-resource "aws_ssoadmin_permission_set_inline_policy" "cipa2" {
-  inline_policy      = file("files/policies/sts-assume-role.json")
+  inline_policy      = file("files/policies/custom-inline-policy.json")
   instance_arn       = aws_ssoadmin_permission_set.cps1.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.cps1.arn
 }
