3562b1eef4f888a9ac77f71a08863d15ce368fd7
root <root@ip-172-31-90-188.ec2.internal>
2022-10-03 16:22:14 +0000

    length of ps name to be in range 1 32

--- devaccone/terraform/custom-sso-admin-permission-set-2.tf
+++ devaccone/terraform/custom-sso-admin-permission-set-2.tf
@@ -1,7 +1,7 @@
 #sso permission set module
 module "sso_permission_set" {
   source = "./modules/sso-permission-sets"
-  aws_ssoadmin_permission_set_name = "ct2-module-sso-admin-permission-set"
+  aws_ssoadmin_permission_set_name = "ct2-mod-sso-admin-permission-set"
   aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/PowerUserAccess"]
   aws_ssoadmin_permission_set_inline_policy_json = "custom-sso-admin-user.json"
 }
