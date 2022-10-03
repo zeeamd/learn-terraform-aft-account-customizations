7aaa95db6eb97a0bcbf3fd431b7976b60863eae0
root <root@ip-172-31-90-188.ec2.internal>
2022-10-03 12:42:15 +0000

    update permission sets

--- devaccone/terraform/custom-s3.tf
+++ devaccone/terraform/custom-s3.tf
@@ -1,9 +1,9 @@
 #This custom file to test account customizations
 
-resource "aws_s3_bucket" "custom-bucket" {
-  bucket = "${data.aws_caller_identity.current.account_id}-donotdelete"
-
-  tags = merge(module.tags.map,{
-   Name = "${data.aws_caller_identity.current.account_id}-donotdelete"
-  })
-}
+#resource "aws_s3_bucket" "custom-bucket" {
+#  bucket = "${data.aws_caller_identity.current.account_id}-donotdelete"
+#
+#  tags = merge(module.tags.map,{
+#   Name = "${data.aws_caller_identity.current.account_id}-donotdelete"
+#  })
+#}
