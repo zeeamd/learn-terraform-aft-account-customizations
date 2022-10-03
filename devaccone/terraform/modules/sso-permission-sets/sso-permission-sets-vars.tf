5e51455ba13357067de6f429080dae5f0dd5ad3b
root <root@ip-172-31-90-188.ec2.internal>
2022-10-03 15:59:52 +0000

    add permission set module

---
+++ devaccone/terraform/modules/sso-permission-sets/sso-permission-sets-vars.tf
@@ -0,0 +1,4 @@
+#sso permission set variables
+variable "aws_ssoadmin_permission_set_name" { default = "UNDEFINED" }
+variable "aws_ssoadmin_managed_policy_attachments_arn" { default = [] }
+variable "aws_ssoadmin_permission_set_inline_policy_json" { default = "UNDEFINED" }
