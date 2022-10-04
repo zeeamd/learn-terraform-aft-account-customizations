5721d69a8ca0c6f7d7132affae2ce66dd977f5b0
root <root@ip-172-31-90-188.ec2.internal>
2022-10-04 07:34:57 +0000

    update module vars

--- devaccone/terraform/custom-s3.tf
+++
@@ -1,9 +0,0 @@
-#This custom file to test account customizations
-
-#resource "aws_s3_bucket" "custom-bucket" {
-#  bucket = "${data.aws_caller_identity.current.account_id}-donotdelete"
-#
-#  tags = merge(module.tags.map,{
-#   Name = "${data.aws_caller_identity.current.account_id}-donotdelete"
-#  })
-#}
--- devaccone/terraform/custom-sso-admin-permission-set-2.tf
+++ devaccone/terraform/custom-sso-admin-permission-set-2.tf
@@ -1,11 +1,12 @@
 #sso permission set module
-module "sso_permission_set" {
+module "sso_permission_sets" {
+  for_each = var.custom-sso-admin-permission-sets
   source = "./modules/sso-permission-sets"
-  aws_ssoadmin_permission_set_name = "ct2-mod-sso-admin-permission-set"
-  aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/PowerUserAccess"]
-  aws_ssoadmin_permission_set_inline_policy_json = "custom-sso-admin-user.json"
-  aws_ssoadmin_permission_set_session_duration = "PT12H"
+  aws_ssoadmin_permission_set_name = custom-sso-admin-permission-sets.value.aws_ssoadmin_permission_set_name
+  aws_ssoadmin_managed_policy_attachments_arn = custom-sso-admin-permission-sets.value.aws_ssoadmin_managed_policy_attachments_arn
+  aws_ssoadmin_permission_set_inline_policy_json = custom-sso-admin-permission-sets.value.aws_ssoadmin_permission_set_inline_policy_json
+  aws_ssoadmin_permission_set_session_duration = custom-sso-admin-permission-sets.value.aws_ssoadmin_permission_set_session_duration
   tags = merge(module.tags.map,{
-   Name = "ct2-mod-sso-admin-permission-set"
+   Name = custom-sso-admin-permission-sets.value.aws_ssoadmin_permission_set_name
   })
 }
