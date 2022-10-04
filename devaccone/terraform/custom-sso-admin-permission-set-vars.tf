0c9b8484dca56f5cd70681ae24d4e55664b3230c
root <root@ip-172-31-90-188.ec2.internal>
2022-10-04 07:56:13 +0000

    update vars

--- devaccone/terraform/custom-sso-admin-permission-set-vars.tf
+++ devaccone/terraform/custom-sso-admin-permission-set-vars.tf
@@ -1,8 +1,10 @@
-custom-sso-admin-permission-sets = [
-{
-  aws_ssoadmin_permission_set_name = "ct2-mod-sso-admin-permission-set"
-  aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/PowerUserAccess"]
-  aws_ssoadmin_permission_set_inline_policy_json = "custom-sso-admin-user.json"
+variable "custom-sso-admin-permission-sets" { 
+ default = [
+  {
+  aws_ssoadmin_permission_set_name = "ct2-mod-sso-admin-permission-set",
+  aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/PowerUserAccess"],
+  aws_ssoadmin_permission_set_inline_policy_json = "custom-sso-admin-user.json",
   aws_ssoadmin_permission_set_session_duration = "PT12H"
+  }
+ ] 
 }
-]
