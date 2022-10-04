94ca9028207b1a4c3faa4a5774a5d92731476b95
root <root@ip-172-31-90-188.ec2.internal>
2022-10-04 16:27:55 +0000

    remove all policies

--- devaccone/terraform/custom-sso-admin-permission-set-vars.tf
+++ devaccone/terraform/custom-sso-admin-permission-set-vars.tf
@@ -2,8 +2,8 @@ variable "custom-sso-admin-permission-sets" {
  default = {
   ps1 = {
   aws_ssoadmin_permission_set_name = "ct-sso-admin-permission-set"
-  aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/AWSSSOMemberAccountAdministrator"]
-  aws_ssoadmin_permission_set_inline_policy_json = "custom-sso-admin-user.json"
+  aws_ssoadmin_managed_policy_attachments_arn = [""]
+  aws_ssoadmin_permission_set_inline_policy_json = ""
   aws_ssoadmin_permission_set_session_duration = "PT12H"
   }  
  }
