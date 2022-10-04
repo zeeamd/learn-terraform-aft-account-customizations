bba85984c110daf49fca9ccb8b83926828abfce1
root <root@ip-172-31-90-188.ec2.internal>
2022-10-04 09:20:43 +0000

    add new permission set

--- devaccone/terraform/custom-sso-admin-permission-set.tf
+++ devaccone/terraform/custom-sso-admin-permission-set.tf
@@ -1,31 +1,12 @@
-data "aws_ssoadmin_instances" "cssoai" {}
-
-output "arn" {
-  value = tolist(data.aws_ssoadmin_instances.cssoai.arns)[0]
+#sso permission set module
+module "sso_permission_sets" {
+  for_each = var.custom-sso-admin-permission-sets
+  source = "./modules/sso-permission-sets"
+  aws_ssoadmin_permission_set_name = each.value["aws_ssoadmin_permission_set_name"]
+  aws_ssoadmin_managed_policy_attachments_arn = each.value["aws_ssoadmin_managed_policy_attachments_arn"]
+  aws_ssoadmin_permission_set_inline_policy_json = each.value["aws_ssoadmin_permission_set_inline_policy_json"]
+  aws_ssoadmin_permission_set_session_duration = each.value["aws_ssoadmin_permission_set_session_duration"]
+  tags = merge(module.tags.map,{
+   Name = each.value["aws_ssoadmin_permission_set_name"]
+  })
 }
-
-output "identity_store_id" {
-  value = tolist(data.aws_ssoadmin_instances.cssoai.identity_store_ids)[0]
-}
-
-#resource "aws_ssoadmin_permission_set" "cssops" {
-#  name = "ct2-sso-admin-permission-set"
-#  instance_arn = tolist(data.aws_ssoadmin_instances.cssoai.arns)[0]
-#  tags = merge(module.tags.map,{
-#   Name = "ct2-sso-admin-permission-set",
-#   Automation = "AFT-Terraform"
-#  })
-#}
-#
-#resource "aws_ssoadmin_managed_policy_attachment" "cssomp" {
-#  for_each           = toset(["arn:aws:iam::aws:policy/PowerUserAccess"])
-#  instance_arn       = tolist(data.aws_ssoadmin_instances.cssoai.arns)[0]
-#  managed_policy_arn = each.key
-#  permission_set_arn = aws_ssoadmin_permission_set.cssops.arn
-#}
-#
-#resource "aws_ssoadmin_permission_set_inline_policy" "cssoip" {
-#  inline_policy      = file("files/policies/custom-sso-admin-user.json")
-#  instance_arn       = aws_ssoadmin_permission_set.cssops.instance_arn
-#  permission_set_arn = aws_ssoadmin_permission_set.cssops.arn
-#}
