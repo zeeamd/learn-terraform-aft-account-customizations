7cf5a625ae542a8ced493483a8744bc95626d0e4
root <root@ip-172-31-90-188.ec2.internal>
2022-10-05 07:18:29 +0000

    update session time

--- devaccone/terraform/custom-sso-admin-permission-set-vars.tf
+++ devaccone/terraform/custom-sso-admin-permission-set-vars.tf
@@ -4,7 +4,7 @@ variable "custom-sso-admin-permission-sets" {
   aws_ssoadmin_permission_set_name = "ct-sso-admin-permission-set"
   aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/AWSSSOMemberAccountAdministrator"]
   aws_ssoadmin_permission_set_inline_policy_json = ""
-  aws_ssoadmin_permission_set_session_duration = "PT12H"
+  aws_ssoadmin_permission_set_session_duration = ""
   }  
  }
 }
