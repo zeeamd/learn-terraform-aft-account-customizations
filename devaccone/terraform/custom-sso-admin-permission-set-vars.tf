cfcfa83dac147f8bebd35e0ab4be765b2fc326ef
root <root@ip-172-31-90-188.ec2.internal>
2022-10-06 11:28:22 +0000

    update permission set nm

--- devaccone/terraform/custom-sso-admin-permission-set-vars.tf
+++ devaccone/terraform/custom-sso-admin-permission-set-vars.tf
@@ -7,7 +7,7 @@ variable "custom-sso-admin-permission-sets" {
   aws_ssoadmin_permission_set_session_duration = "PT8H"
   }  
   ps2 = {
-  aws_ssoadmin_permission_set_name = "ct-compliance-audit-permission-set"
+  aws_ssoadmin_permission_set_name = "ct-compliance-audit-ps"
   aws_ssoadmin_managed_policy_attachments_arn = ["arn:aws:iam::aws:policy/PowerUserAccess"]
   aws_ssoadmin_permission_set_inline_policy_json = "custom-compliance-audit.json"
   aws_ssoadmin_permission_set_session_duration = ""
