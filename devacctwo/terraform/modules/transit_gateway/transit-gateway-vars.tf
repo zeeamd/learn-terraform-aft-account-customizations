4109be5339c44c5e8843374a5b7e2627beab0a54
root <root@ip-172-31-90-188.ec2.internal>
2022-10-11 19:00:49 +0000

    add tgw mod

---
+++ devacctwo/terraform/modules/transit_gateway/transit-gateway-vars.tf
@@ -0,0 +1,8 @@
+variable "name" { default = "" }
+variable "amazon_side_asn" { default = null }
+variable "auto_accept_shared_attachments" { default = "" }
+variable "default_route_table_association" { default = "" }
+variable "default_route_table_propagation" { default = "" }
+variable "dns_support" { default = "" }
+variable "vpn_ecmp_support" { default = "" }
+variable "tags" { default = {} }
