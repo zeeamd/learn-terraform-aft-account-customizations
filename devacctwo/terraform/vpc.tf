5804f2fb48c4a72f241cb8a22a19dec10c088f8c
root <root@ip-172-31-90-188.ec2.internal>
2022-10-11 13:52:25 +0000

    test tgw v2

--- devacctwo/terraform/vpc.tf
+++ devacctwo/terraform/vpc.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "ct2_vpc" {
 resource "aws_subnet" "pub1" {
   vpc_id     = aws_vpc.ct2_vpc.id
   cidr_block = "12.2.0.0/28"
+  availability_zone = "us-east-1a"
   tags       = merge(module.tags.map,{
                      Name = "pub1"
                      })
@@ -17,6 +18,7 @@ resource "aws_subnet" "pub1" {
 resource "aws_subnet" "pri1" {
   vpc_id     = aws_vpc.ct2_vpc.id
   cidr_block = "12.2.0.16/28"
+  availability_zone = "us-east-1b"
   tags       = merge(module.tags.map,{
                      Name = "pri1"
                      })
@@ -24,7 +26,6 @@ resource "aws_subnet" "pri1" {
 
 resource "aws_route_table" "rt_pub1" {
   vpc_id = aws_vpc.ct2_vpc.id
-  availability_zone = "us-east-1a"
   tags   = merge(module.tags.map,{
                  Name = "rt_pub1"
                  })
@@ -32,7 +33,6 @@ resource "aws_route_table" "rt_pub1" {
 
 resource "aws_route_table" "rt_pri1" {
   vpc_id = aws_vpc.ct2_vpc.id
-  availability_zone = "us-east-1b"
   tags   = merge(module.tags.map,{
                  Name = "rt_pri1"
                  })
