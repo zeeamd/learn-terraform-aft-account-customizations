d32823df2c08eb59c8384ccf9a6fd1384e8e37e6
root <root@ip-172-31-90-188.ec2.internal>
2022-10-12 13:33:10 +0000

    update vars

--- devacctwo/terraform/modules/vpc/vpc-vars.tf
+++ devacctwo/terraform/modules/vpc/vpc-vars.tf
@@ -1,4 +1,5 @@
 variable "cidr_block" { default = "" }
+variable "vpc_name" { default = "" }
 variable "instance_tenancy" { default = "default" }
 variable "enable_dns_support" { default = true }
 variable "enable_dns_hostnames" { default = true }
