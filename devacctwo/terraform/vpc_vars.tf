0de1c69d95e7d91b34b0af00bbe2df295c5d67c4
root <root@ip-172-31-90-188.ec2.internal>
2022-10-18 11:14:34 +0000

    add vpc

--- devacctwo/terraform/vpc_vars.tf
+++ devacctwo/terraform/vpc_vars.tf
@@ -9,5 +9,14 @@ variable "vpc" {
   cidr_subnet_public = ["12.1.0.0/25","12.1.0.192/26"]
   cidr_subnet_private = ["12.1.0.16/28"]
   }
+  vpc2 = {
+  vpc_name = "ct2"
+  vpc_cidr = "13.2.0.0/27"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  cidr_subnet_public = ["13.2.0.0/28"]
+  cidr_subnet_private = ["13.2.0.16/28"]
+  }
  }
 }
